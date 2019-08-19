a084cce33aefe6ff8ad63e6a4aae99969b986807
weixiang.liu <weixiang.liu@ubtrobot.com>
2019-08-19 19:49:18 +0800

    add Extention file

---
+++ Example/Pods/Target Support Files/Pods-UBTKit_Tests/Pods-UBTKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UBTKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_UBTKit_Tests
+@end
---
+++ Example/Pods/Target Support Files/UBTKit/UBTKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_UBTKit : NSObject
+@end
+@implementation PodsDummy_UBTKit
+@end
